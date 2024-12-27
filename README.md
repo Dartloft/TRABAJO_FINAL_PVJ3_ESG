f2ee983670a3ec91fd033543c55be894ccb2620a
Emanuel Suares Gonzalez <esuaresgonzalez@gmail.com>
2024-12-27 18:34:04 -0300

    COMMIT
    Juego PV3 Final

---
+++ README.md
@@ -0,0 +1,3 @@
+# suares_gonzalez_emanuel_act_5
+ 
+https://drive.google.com/file/d/16ZF3CAc8cYzYT5MNrua86qzatIgQRQdn/view?usp=drive_link
---
+++ bullet.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var SPEED = 300
+
+func _physics_process(delta):
+	global_position.y += -SPEED * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_area_entered(area):
+	if area is Enemy:
+		area.die()
+		Signals.emit_signal("on_score_increment",10 )
+		queue_free()
+		$explode.playing = true
+	
+	if area is Powerup:
+		area.die()
+		Signals.emit_signal("on_score_increment",5 )
+		queue_free()
+		$explode.playing = true	
+		
+		
